90bc26eda14058346b32d2ba86387b6bd1dcfbb3
Alex Sambvani <alexsambvani@gmail.com>
2017-05-22 22:07:02 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+
+def prime?(number)
+  counter = 2
+  if number < 1 || number == 1
+    return false
+  end
+
+  while counter <= Math.sqrt(number.abs)
+    if number.abs % counter == 0
+      return false
+      break
+    end
+    counter += 1
+  end
+  return true
+end
